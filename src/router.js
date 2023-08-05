693f71b26e01697f106259d5f5ebb5ef0a9cf11e
Alan Vale <valealan84@gmail.com>
2023-08-05 11:00:06 -0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 const fileController = require('./controller/file')
 
 const router = new Router()
-const upload = multer({limits: {fileSize: 1048576}}) // 10MB
+const upload = multer({limits: {fileSize: 20 * 1024 ** 2}}) // 20MB
 
 router.get('/', (req, res) => res.sendFile(`${process.cwd()}/public/index.html`))
 router.post('/file', upload.single('file'), fileController.postFile)
