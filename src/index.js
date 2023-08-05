1f8306c7aefa5c67e7a20a9967edde9de99613bd
Alan Vale <valealan84@gmail.com>
2023-08-05 11:22:34 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const router = require('./router.js')
+const path = require('path')
 
 const app = express()
 const port = 666
 
-const path = require('path');
 app.use(express.static(path.join(process.cwd(), 'public')))
 
 app.use(cors())
