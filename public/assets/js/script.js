ea3789840bcfa8a3097befba65e6fc08be373406
Alan Vale <valealan84@gmail.com>
2024-07-30 18:17:45 -0300

    refactor: general

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,31 +1,22 @@
-const form = document.querySelector('form')
-const loading = document.querySelector('.loading')
+const button  = document.querySelector('input[type=submit]')
+const form    = document.querySelector('form')
 
-form.onsubmit = e => {
+form.onsubmit = async e => {
   e.preventDefault()
 
-  form.enctype = 'multipart/form-data'
-  const formData = new FormData(form)
+  button.disabled = true
 
-  loading.style.display = 'inline'
+  const res = await fetch('/file', { method: 'POST', body: new FormData(form) })
+  const data = await res.blob()
+  const file = new Blob([data], { type: 'application/octet-stream' })
+  const url = window.URL.createObjectURL(file)
 
-  fetch('/file', {
-    method: 'POST',
-    body: formData
-  })
-    .then(res => res.blob())
-    .then(data => {
-      const file = new Blob([data], { type: 'application/octet-stream' })
-      const fileURL = window.URL.createObjectURL(file)
-      
-      const anchor = document.createElement('a')
-      anchor.href = fileURL
-      anchor.download = 'steam-ws.zip'
-      
-      anchor.click()
-      
-      anchor.remove()
-      window.URL.revokeObjectURL(fileURL)
-      loading.style.display = 'none'
-    })
+  const anchor = document.createElement('a')
+  anchor.href = url
+  anchor.download = 'steam-ws.zip'
+
+  anchor.click()
+  anchor.remove()
+  window.URL.revokeObjectURL(url)
+  button.disabled = false
 }
